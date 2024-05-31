21dde47da7927cd81d33ea988ac6f8e187e70a31
nigam <nigammishra826@gmail.com>
2024-05-31 23:08:37 +0530

    project add

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,6 +7,10 @@ import Particle from "../Particle";
 import artificial from "../../Assets/Projects/artificial intelligence.png";
 import librarymanagementsystem from "../../Assets/Projects/librarymanagementsystem.png";
 import tourismmanagementsystem from "../../Assets/Projects/tourismmanagementsystem.png";
+import easyshop from "../../Assets/Projects/EasyShop.png";
+import freshcart from "../../Assets/Projects/Freshcart.png";
+import employeemanagementsystem from "../../Assets/Projects/employeemanagementsystem.png";
+import contactbook from "../../Assets/Projects/contactbook.png";
 // import chatify from "../../Assets/Projects/chatify.png";
 // import suicide from "../../Assets/Projects/suicide.png";
 // import bitsOfCode from "../../Assets/Projects/blog.png";
@@ -28,7 +32,7 @@ function Projects() {
               imgPath={tourismmanagementsystem}
               isBlog={false}
               title="Tourism Management System"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              description="This  website  embodies  the  essence  of  the  Odisha  Tourism  System,  featuring  fullresponsiveness.  It  comprehensively  showcases  all  the  tourist  destinations  acrossOdisha.  Technologies utilized in its development include React, Bootstrap, JavaScript, andReact-Bootstrap."
               ghLink="https://github.com/nigammishra/tourism-management-system.git"
               demoLink="https://nigammishra.github.io/tourism-management-system/"
             />
@@ -39,7 +43,7 @@ function Projects() {
               imgPath={artificial}
               isBlog={false}
               title="About Artificial Intelligence"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
+              description="This static project, comprising approximately 120 web pages, provides comprehensive information about artificial intelligence, including its benefits, drawbacks, and merits. The project is built using HTML, CSS, and JavaScript."
               ghLink="https://github.com/nigammishra/Artificial-intelligence.git"
               // demoLink="https://blogs.soumya-jit.tech/"
             />
@@ -50,45 +54,54 @@ function Projects() {
               imgPath={librarymanagementsystem}
               isBlog={false}
               title="Library Management System"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              description="It's  a  full-stack  project  where  admins  manage  CRUD  operations.  Data  is  storedlocally, with login and registration data sourced from session storage.Technologies utilized in its development include React, Bootstrap, JavaScript, andReact-Bootstrap."
               ghLink="https://github.com/frex-arup/library-mgmt-react.git"
               demoLink="https://frex-arup.github.io/library-mgmt-react/"              
             />
           </Col>
 
-          {/* <Col md={4} className="project-card">
+          <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={freshcart}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Fresh Cart"
+              description="This vibrant and animated grocery shop frontend design is crafted with a seamless blend of HTML, CSS, and JavaScript."
+              ghLink="https://github.com/nigammishra/grocery-shop.git"
+              // demoLink="https://plant49-ai.herokuapp.com/"
             />
-          </Col> */}
+          </Col>
 
-          {/* <Col md={4} className="project-card">
+          <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={easyshop}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="Easy Shop"
+              description="It is a fully responsive front-end design for a shopping website featuring a wide variety of items, similar to Flipkart and Amazon."
+              ghLink="https://github.com/nigammishra/EasyShop.git"
+              demoLink="https://nigammishra.github.io/EasyShop/" 
             />
-          </Col> */}
+          </Col>
 
-          {/* <Col md={4} className="project-card">
+          <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={employeemanagementsystem}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              title="Employee Management System"
+              description="This React project uses a JSON server as the backend and database, enabling CRUD operations via the server's base URL. The frontend is built with React.js and Bootstrap, supporting API integration for GET and POST methods to retrieve and submit data."
+              ghLink="https://github.com/nigammishra/employee-management-systemm.git"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
-          </Col> */}
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={contactbook}
+              isBlog={false}
+              title="Employee Management System"
+              description="This React project uses a JSON server as the backend and database, enabling CRUD operations via the server's base URL. The frontend is built with React.js and Bootstrap, supporting API integration for GET and POST methods to retrieve and submit data."
+              ghLink="https://github.com/frex-arup/contact-book-react.git"
+              demoLink="https://frex-arup.github.io/contact-book-react/"     
+            />
+          </Col>
         </Row>
       </Container>
     </Container>
