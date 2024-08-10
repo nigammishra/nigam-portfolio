5a2b0ac3911a57be12583798ed9f12a0c246e2b2
nigam <nigammishra826@gmail.com>
2024-08-10 13:10:53 +0530

    logo updated and new project added and updated

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -11,6 +11,7 @@ import easyshop from "../../Assets/Projects/EasyShop.png";
 import freshcart from "../../Assets/Projects/Freshcart.png";
 import employeemanagementsystem from "../../Assets/Projects/employeemanagementsystem.png";
 import contactbook from "../../Assets/Projects/contactbook.png";
+import portfolio2 from "../../Assets/Projects/portfolio2.png";
 // import chatify from "../../Assets/Projects/chatify.png";
 // import suicide from "../../Assets/Projects/suicide.png";
 // import bitsOfCode from "../../Assets/Projects/blog.png";
@@ -37,7 +38,28 @@ function Projects() {
               demoLink="https://nigammishra.github.io/tourism-management-system/"
             />
           </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={portfolio2}
+              isBlog={false}
+              title="Animated Portfolio"
+              description="This is a fully responsive HTML static portfolio template."
+              ghLink="https://github.com/nigammishra/animatedportfolio.git"
+              demoLink="https://nigammishra.github.io/animatedportfolio/"
+            />
+          </Col>
 
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={freshcart}
+              isBlog={false}
+              title="Fresh Cart"
+              description="This self-initiated project is a fully responsive website, similar to any online grocery store. The primary language used is React.js, with additional technologies including HTML, CSS, Bootstrap, React-Bootstrap, and JavaScript."
+              ghLink="https://github.com/nigammishra/grocery-react.git"
+              demoLink="https://nigammishra.github.io/Grocery-react/"
+            />
+          </Col>
+          
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={artificial}
@@ -56,18 +78,7 @@ function Projects() {
               title="Library Management System"
               description="It's  a  full-stack  project  where  admins  manage  CRUD  operations.  Data  is  storedlocally, with login and registration data sourced from session storage.Technologies utilized in its development include React, Bootstrap, JavaScript, andReact-Bootstrap."
               ghLink="https://github.com/frex-arup/library-mgmt-react.git"
-              demoLink="https://frex-arup.github.io/library-mgmt-react/"              
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={freshcart}
-              isBlog={false}
-              title="Fresh Cart"
-              description="This vibrant and fully responsive grocery shop frontend, crafted using React.js alongside HTML, CSS, and JavaScript, features seamless animations and an engaging user experience."
-              ghLink="https://github.com/nigammishra/grocery-shop.git"
-              // demoLink="https://plant49-ai.herokuapp.com/"
+              demoLink="https://frex-arup.github.io/library-mgmt-react/"
             />
           </Col>
 
@@ -78,7 +89,7 @@ function Projects() {
               title="Easy Shop"
               description="It is a fully responsive front-end design for a shopping website featuring a wide variety of items, similar to Flipkart and Amazon."
               ghLink="https://github.com/nigammishra/EasyShop.git"
-              demoLink="https://nigammishra.github.io/EasyShop/" 
+              demoLink="https://nigammishra.github.io/EasyShop/"
             />
           </Col>
 
@@ -89,17 +100,17 @@ function Projects() {
               title="Employee Management System"
               description="This React project uses a JSON server as the backend and database, enabling CRUD operations via the server's base URL. The frontend is built with React.js and Bootstrap, supporting API integration for GET and POST methods to retrieve and submit data."
               ghLink="https://github.com/nigammishra/employee-management-systemm.git"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
             />
           </Col>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={contactbook}
               isBlog={false}
-              title="Employee Management System"
-              description="This React project uses a JSON server as the backend and database, enabling CRUD operations via the server's base URL. The frontend is built with React.js and Bootstrap, supporting API integration for GET and POST methods to retrieve and submit data."
+              title="Contact Book"
+              description="In this React.js project, all contacts are saved in a JSON server, and CRUD operations—including create, read, update, and delete—are performed on the data."
               ghLink="https://github.com/frex-arup/contact-book-react.git"
-              demoLink="https://frex-arup.github.io/contact-book-react/"     
+              demoLink="https://frex-arup.github.io/contact-book-react/"
             />
           </Col>
         </Row>
